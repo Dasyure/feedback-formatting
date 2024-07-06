14eea8e16c6c40b3ca96a938da15516e3be6f604
Daysure <sandeep.das@student.unsw.edu.au>
2024-07-07 01:11:45 +1000

    feat: using classes to store feedback

--- formatter.py
+++ formatter.py
@@ -7,9 +7,9 @@ Instructions:
 '''
 import csv
 import copy
-import re
 INPUT_FILE = "input.csv"
-OUTPUT_FILE = 'results.txt'
+START_AND_END_MSG = "iter1_message.md"
+OUTPUT_FILE = 'results.md'
 HASH_STR = '####'
 
 '''
@@ -26,6 +26,62 @@ individual_results = { name1: results_object, }
 '''
 results = {}
 individual_results = {}
+feedback = {
+    'group': {},
+    'individual': {}
+}
+
+class Feedback:
+    '''
+    self.feedback = {
+        category1: {
+            mark: Perfect,
+            comments: Not bad!
+        },
+    }
+    '''
+    def __init__(self) -> None:
+        self.feedback = {}
+    def is_empty(self):
+        return True if self.feedback == {} else False
+    '''
+    Description: populates itself with the categories
+    '''
+    def add_categories(self, categories) -> None:
+        for category in categories:
+            if category not in ['', HASH_STR]:
+                self.feedback[category] = { 'mark': '', 'comments': ''}
+    '''
+    Description: returns list of categories
+    '''
+    def get_categories(self):
+        return [category for category in self.feedback.keys()]
+    '''
+    Description: populates itself with the comments and mark for each category
+    '''
+    def add_marks_comments(self, row):
+        categories = self.get_categories()
+        max_index = len(categories) * 2 - 1
+        category_index = 0
+        curr_category = categories[0]
+        curr_section = 'mark'
+        for i, mark_or_comment in enumerate(row):
+            curr_section = 'mark' if i % 2 == 0 else 'comments'
+            self.feedback[curr_category][curr_section] = mark_or_comment
+            # If index is odd, change categories
+            if i % 2 != 0 and i < max_index:
+                category_index += 1
+                curr_category = categories[category_index]
+            if i == max_index:
+                break
+    def print(self, raw = False) -> None:
+        if raw:
+            print(self.feedback)
+        else:
+            for category in self.feedback:
+                print(f"  {category}:\n"
+                    f"    • Mark: {self.feedback[category]['mark']}\n"
+                    f"    • Comments: {self.feedback[category]['comments']}")
 
 '''
 Description: populates results with the categories
@@ -48,7 +104,7 @@ def populate_marks_comments(row):
         curr_section = 'mark' if i % 2 == 0 else 'comments'
         results[curr_category][curr_section] = item
         # If index is odd, change categories
-        if i % 2 != 0 and i != max_index:
+        if i % 2 != 0 and i < max_index:
             category_counter += 1
             curr_category = keys[category_counter]
         if i == max_index:
@@ -77,7 +133,7 @@ def populate_marks_comments_individual(row, results_template):
         curr_section = 'mark' if i % 2 == 0 else 'comments'
         results_template[curr_category][curr_section] = item
         # If index is odd, change categories
-        if i % 2 != 0 and i != max_index:
+        if i % 2 != 0 and i < max_index:
             category_counter += 1
             curr_category = keys[category_counter]
         if i == max_index:
@@ -96,35 +152,58 @@ Description: parses CSV input into data structures (results and individual_resul
     Name | Mark | Description | Mark | Description | Mark | Description
     Name | Mark | Description | Mark | Description | Mark | Description
 '''
-def copy_input():
+def parse_csv():
+    global feedback
     counter = 0
     with open(INPUT_FILE, 'r') as csvfile:
-        csv_reader = csv.reader(csvfile, delimiter='\t')
+        csv_reader = csv.reader(csvfile, delimiter=',')
         # GROUP: First two rows
+        Group_feedback = Feedback()
         for row in csv_reader:
             counter += 1 if counter < 2 else None
-            if (results == {}):
-                populate_categories(row)
+            if (Group_feedback.is_empty()):
+                # populate_categories(row)
+                Group_feedback.add_categories(row)
             else:
-                populate_marks_comments(row)
+                # populate_marks_comments(row)
+                Group_feedback.add_marks_comments(row)
             if (counter >= 2):
                 break
 
         # INDIVIDUAL: Continues looping from 3rd row onwards
         list_of_names = []
-        list_of_results = []
-        template_individual_results = {}
+        list_of_feedback = []
+        individual_feedback = Feedback()
         for row in csv_reader:
             if (row[0] == HASH_STR):
-                template_individual_results = populate_categories_individual(row)
-            elif(row[0] == ''):
-                continue
-            else:
+                individual_feedback = Feedback() # create new feedback each row
+                individual_feedback.add_categories(row)
+            elif(row[0] != ''):
                 list_of_names.append(row[0])
-                feedback = populate_marks_comments_individual(row[1:], template_individual_results)
-                list_of_results.append(copy.deepcopy(feedback))
+                individual_feedback.add_marks_comments(row[1:])
+                list_of_feedback.append(copy.deepcopy(individual_feedback))
         for i, name in enumerate(list_of_names):
-            individual_results[name] = list_of_results[i]
+            feedback['individual'][name] = list_of_feedback[i]
+
+'''
+Description: grabs the start_msg to add to the beginning of the output, then
+    grab the end_msg to add to the end of the output. 
+'''
+def get_start_end_messages():
+    with open(START_AND_END_MSG, 'r') as textfile:
+        start_message = ''
+        end_message = ''
+        flag = 'START'
+        for line in textfile:
+            if 'START_MESSAGE' in line:
+                flag = 'START'
+                continue
+            elif 'END_MESSAGE' in line:
+                flag = 'END'
+                continue
+            start_message += line if flag == 'START' else ''
+            end_message += line if flag == 'END' else ''
+    return [start_message, end_message]
 
 '''
 Description: formats the final output
@@ -132,36 +211,27 @@ Description: formats the final output
 def export_results(start_message, end_message):
     with open(OUTPUT_FILE, 'w') as textfile:
         # Start message
-        textfile.write(f"{start_message[1:]}\n")
-        # Group and Individual feedback
+        textfile.write(f"{start_message}")
+        # Group feedback
+        group_result = ''
         for category in results:
             comments = "Nothing to comment on!" if results[category]['comments'] == '' else results[category]['comments']
-            textfile.write(f"[{results[category]['mark']}] {category}: {comments}\n")
-        textfile.write('\n** Additional individual comments **\n\n')
+            group_result += f"  • [{results[category]['mark']}] {category}: {comments} \\\n"
+        textfile.write(group_result[:-2])
+
+        # Individual feedback
+        individual_result = ''
+        textfile.write('\n\n**Additional individual comments** \\\n')
         for name in individual_results:
-            textfile.write(f"{name}:\n")
+            individual_result += f"{name}: \\\n"
             for cat in individual_results[name]:
                 comments = "Nothing to comment on!" if individual_results[name][cat]['comments'] == '' else individual_results[name][cat]['comments']
-                textfile.write(f"  • [{individual_results[name][cat]['mark']}] {cat}: {comments}\n")
+                individual_result += f"  • [{individual_results[name][cat]['mark']}] {cat}: {comments} \\\n"
+        textfile.write(individual_result[:-2])
         # End message
-        textfile.write(f"\n{end_message[1:]}")
-
-
+        textfile.write(f"\n\n{end_message}")
 
 if __name__ == "__main__":
-    start_message = """
-Congrats on finishing iter1! Here’s my overall thoughts on how you did this iteration:
-  • 
-  • Reminder: I was lenient with marking this iteration, so make sure you read each comment even if I gave a perfect mark. 
-
-Below is a very quick breakdown of each section of the manual mark portion of iter1. The majority of these marks come from style and git practices! If a section was done perfectly, I won’t have many comments. The marks range from [terrible/below average/average/good/great/perfect]:
-"""
-    end_message = """
-Additional notes:
-  • Please read any comments left in the feedback merge request. It should be called “Project Feedback: Iteration 1”. If you click on the “changes” tab and scroll through the unresolved threads, you can view my comments. If I notice a line of code with bad style, I only comment on the first instance of it, so make sure you apply the feedback everywhere. 
-  • Final marks: the course will release these. If you find your marks differ slightly from your team mates, it’s completely normal! It’s due to the way peer review data is collected by the course. 
-  • However, if your marks are significantly less than your team mates, I will message you individually on MS teams with reasons why. 
-Goodluck with iter2, see you all in two weeks!
-"""
-    copy_input()
-    export_results(start_message, end_message)
+    parse_csv()
+    # [start_message, end_message] = get_start_end_messages()
+    # export_results(start_message, end_message)
